63d19967543dcda28098fe66b84b8f0313998f66
Anna Ndiaye <108986417+Annaa4@users.noreply.github.com>
2022-09-08 20:35:59 +0000

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -18,15 +18,3 @@ int main(void)
   puts("\"Programming is like building a multilingual puzzle");
   
   return (0);
-  
-=======
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
->>>>>>> 3bf57755f912f65792ba762f936fb8e9f50507ce
-}
