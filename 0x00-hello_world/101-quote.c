5abeaf16aba27f056e5b668df0230b1a6051b1fa
Anna Ndiaye <108986417+Annaa4@users.noreply.github.com>
2022-09-08 20:49:13 +0000

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,26 +1,4 @@
 #include <stdio.h>
-<<<<<<< HEAD
-/**
-
- * main - Entry point
-
- *
-
- * Return: Always 0 (Success)
-
- */
-
-int main(void)
-  
-{
-  
-  write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
-  
-
-  
-  return (1);
-  
-=======
 
 /**
  * main - Entry point
@@ -32,5 +10,4 @@ int main(void)
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
 
 	return (1);
->>>>>>> 452b9da2914b4ace271d3f7d703f2eda96356b41
 }
