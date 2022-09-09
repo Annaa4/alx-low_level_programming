310a19b908d319a977583412036bb9d77e35257a
Anna Ndiaye <108986417+Annaa4@users.noreply.github.com>
2022-09-09 21:50:24 +0000

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,59 +1,4 @@
 #include <stdio.h>
-<<<<<<< HEAD
-
-#include <stdlib.h>
-
-#include <time.h>
-
-/**
-
- * main - Entry point
-
- *
-
- * Return: Always 0 (Success)
-
- */
-
-int main(void)
-  
-{
-  
-  int d;
-  
-
-  
-  for (d = 0; d < 90; d++)
-    
-    {
-      
-      for ()
-	
-	{
-	  
-	  putchar((d / 10) + '0');
-	  
-	  putchar((d % 10) + '0');
-	  
-	}
-      
-      if (d != 89)
-	
-	{
-	  
-	  putchar(',');
-	  
-	  putchar(' ');
-	  
-	}
-      
-    }
-  
-  putchar('\n');
-  
-  return (0);
-  
-=======
 #include <stdlib.h>
 #include <time.h>
 /**
@@ -80,5 +25,4 @@ int main(void)
 	}
 	putchar('\n');
 	return (0);
->>>>>>> 8214337f04e698e6a3494ec8d19caf450bf6c4de
 }
