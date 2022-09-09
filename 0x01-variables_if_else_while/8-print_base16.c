164f3c289a8c06cdbde19af10a636c366ba3e730
Anna Ndiaye <108986417+Annaa4@users.noreply.github.com>
2022-09-09 21:25:13 +0000

    Update 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,47 +1,5 @@
 #include <stdio.h>
 
-<<<<<<< HEAD
-
-
-/**
-
- * main - Prints all the numbers of base 16 in lowercase.
-
- *
-
- * Return: Always 0.
-
- */
-
-int main(void)
-  
-{
-  
-  int num;
-  
-  char letter;
-  
-
-  
-  for (num = 0; num < 10; num++)
-    
-    putchar((num % 10) + '0');
-  
-
-  
-  for (letter = 'a'; letter <= 'f'; letter++)
-    
-    putchar(letter);
-  
-
-  
-  putchar('\n');
-  
-
-  
-  return (0);
-  
-=======
 /**
  * main - Prints all the numbers of base 16 in lowercase.
  *
@@ -61,5 +19,4 @@ int main(void)
 	putchar('\n');
 
 	return (0);
->>>>>>> 94c8681996f7022cfee93a46bc003839b2c29e74
 }
