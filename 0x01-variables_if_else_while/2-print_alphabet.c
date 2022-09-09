5f6972ecaedda1266f2d26d83ab37427f4b29451
Anna Ndiaye <108986417+Annaa4@users.noreply.github.com>
2022-09-09 21:05:17 +0000

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,39 +1,4 @@
 #include <stdio.h>
-<<<<<<< HEAD
-
-#include <stdlib.h>
-
-#include <time.h>
-
-/**
-
- * main - Entry point
-
- *
-
- * Return: Always 0 (Success)
-
- */
-
-int main(void)
-  
-{
-  
-  char low;
-  
-
-  
-  for (low = 'a'; low <= 'z'; low++)
-    
-    putchar(low);
-  
-  printf("\n");
-  
-
-  
-  return (0);
-  
-=======
 #include <stdlib.h>
 #include <time.h>
 /**
@@ -50,5 +15,4 @@ int main(void)
 		printf("\n");
 
 	return (0);
->>>>>>> d4d2b86b9100b90e07cab3a55f250e1dea9ae8f4
 }
