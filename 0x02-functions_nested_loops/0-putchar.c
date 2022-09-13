6682f501a3133e27cbd7ec1ec13d6da3a69c1630
Anna Ndiaye <108986417+Annaa4@users.noreply.github.com>
2022-09-13 13:20:37 +0000

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,47 +1,10 @@
 #include "holberton.h"
-<<<<<<< HEAD
-
-/**
-
- *main - prints Holberton
-
- *Return: 0
-
- */
-
-=======
 /**
 *main - prints Holberton
 *Return: 0
 */
->>>>>>> 44adaed337bb2d894e70f047a18b47ba1e27d2d3
 int main(void)
 {
-<<<<<<< HEAD
-  
-  _putchar('H');
-  
-  _putchar('o');
-  
-  _putchar('l');
-  
-  _putchar('b');
-  
-  _putchar('e');
-  
-  _putchar('r');
-  
-  _putchar('t');
-  
-  _putchar('o');
-  
-  _putchar('n');
-  
-  _putchar('\n');
-  
-  return (0);
-  
-=======
 	_putchar('H');
 	_putchar('o');
 	_putchar('l');
@@ -53,5 +16,4 @@ int main(void)
 	_putchar('n');
 	_putchar('\n');
 	return (0);
->>>>>>> 44adaed337bb2d894e70f047a18b47ba1e27d2d3
 }
