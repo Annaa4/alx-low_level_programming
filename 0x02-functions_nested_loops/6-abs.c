853112871439da0da9c294193cb54141c24b41c0
Anna Ndiaye <108986417+Annaa4@users.noreply.github.com>
2022-09-13 16:21:47 +0000

    Update 6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,38 +1,4 @@
-<<<<<<< HEAD
 include "main.h"
-
-/**
-
- * _abs - compute the absolute value of an integer
-
- * @n: int type number
-
- * Return: absolute value of @n
-
- */
-
-int _abs(int n)
-
-{
-  
-  if (n < 0)
-    
-    {
-      
-      return (n * -1);
-      
-    }
-  
-  else
-    
-    {
-      
-      return (n);
-      
-    }
-  
-=======
-#include "main.h"
 /**
  * _abs - compute the absolute value of an integer
  * @n: int type number
@@ -48,5 +14,4 @@ int _abs(int n)
 	{
 		return (n);
 	}
->>>>>>> 0a133b9c5b3caaa974dfb308a609144e447d3975
 }
