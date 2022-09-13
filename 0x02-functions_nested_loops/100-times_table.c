f2384fedfbc189c05b4a29267b2d5c2b3cf7a9d9
Anna Ndiaye <108986417+Annaa4@users.noreply.github.com>
2022-09-13 16:46:33 +0000

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,25 +1,4 @@
 #include "main.h"
-<<<<<<< HEAD
-
-/**
-
- * print_times_table - Entry point
-
- * @n: input
-
- * Return: Always 0 (Success)
-
- */
-
-void print_times_table(int n)
-  
-{
-  
-
-  
-  return (0);
-  
-=======
 /**
  * print_times_table - Entry point
  * @n: input
@@ -29,5 +8,4 @@ void print_times_table(int n)
 {
 
 	return (0);
->>>>>>> 368a0fcaba416c39e306b5d2a3b1c7610f1064f2
 }
