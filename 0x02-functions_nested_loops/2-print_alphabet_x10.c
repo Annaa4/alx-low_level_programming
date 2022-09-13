659926f4b8419bc9d59e8759648786e7e4dc01b6
Anna Ndiaye <108986417+Annaa4@users.noreply.github.com>
2022-09-13 16:06:51 +0000

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,47 +1,4 @@
 #include "main.h"
-<<<<<<< HEAD
-
-/**
-
- * print_alphabet_x10 - Print alphabet in lowercase 10 times
-
- * Return: 0
-
- */
-
-void print_alphabet_x10(void)
-  
-{
-  
-  int i = 0;
-  
-  char c;
-  
-
-  
-  while (i < 10)
-    
-    {
-      
-      c = 'a';
-      
-      while (c <= 'z')
-	
-	{
-	  
-	  _putchar(c);
-	  
-	  c++;
-	  
-	}
-      
-      _putchar('\n');
-      
-      i++;
-      
-    }
-  
-=======
 /**
  * print_alphabet_x10 - Print alphabet in lowercase 10 times
  * Return: 0
@@ -62,5 +19,6 @@ void print_alphabet_x10(void)
 		_putchar('\n');
 		i++;
 	}
->>>>>>> 76f4e39702ee023694e315b382b9335c57d1cfa6
 }
+Footer
+© 2022 GitHub, Inc.
