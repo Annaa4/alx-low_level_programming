3f397ccc76a129c7fa60f00e90e20783b49bf88a
Anna Ndiaye <108986417+Annaa4@users.noreply.github.com>
2022-09-13 14:23:54 +0000

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,51 +1,12 @@
 #include "_putchar.h"
-<<<<<<< HEAD
-
-#include "main.h"
-
-/**
-
- * main - Entry point
-
- *
-
- * Return: Always 0 (Success)
-
- */
-
-=======
 #include "main.h"
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
->>>>>>> 9d181700bfc44dac22a12f0e698c8d687d66c2f5
 int main(void)
-  
 {
-<<<<<<< HEAD
-  
-  char *sh = "_putchar";
-  
-  
-  while (*sh)
-    
-    {
-      
-      _putchar(*sh);
-      
-      sh++;
-      
-    }
-  
-  _putchar('\n');
-  
-
-  
-  return (0);
-  
-=======
 	char *sh = "_putchar";
 
 	while (*sh)
@@ -56,5 +17,4 @@ int main(void)
 	_putchar('\n');
 
 	return (0);
->>>>>>> 9d181700bfc44dac22a12f0e698c8d687d66c2f5
 }
