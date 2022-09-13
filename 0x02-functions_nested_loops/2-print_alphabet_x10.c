d1ba99e249e0b4b44492ebf73480bd3f196cea5b
Anna Ndiaye <108986417+Annaa4@users.noreply.github.com>
2022-09-13 13:40:36 +0000

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,32 +1,3 @@
-<<<<<<< HEAD
-#include "holberton.h"
-
-
-
-/**
-
- * print_alphabet - prints the alphabet in lowercase, followed by a new line.
-
- */
-
-void print_alphabet(void)
-  
-{
-  
-  char letter;
-  
-
-  
-  for (letter = 'a'; letter <= 'z'; letter++)
-    
-    _putchar(letter);
-  
-
-  
-  _putchar('\n');
-  
-=======
-
 #include "holberton.h"
 
 /**
@@ -40,5 +11,4 @@ void print_alphabet(void)
 		_putchar(letter);
 
 	_putchar('\n');
->>>>>>> 040c21739f44b36df85e1b26404351022907d429
 }
