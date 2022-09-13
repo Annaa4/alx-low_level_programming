eb4643ae2dab34f522bdce5328299f58d42f5268
Anna Ndiaye <108986417+Annaa4@users.noreply.github.com>
2022-09-13 13:05:24 +0000

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,37 +1,5 @@
 #include "alx.h"
-<<<<<<< HEAD
-
 /**
-
- * Return: 0 on success.
-
- */
-
-int main(void)
-  
-{
-  
-  char c[] = "Alx";
-  
-  int i;
-  
-
-  
-  for (i = 0; i < 9; i++)
-    
-    {
-      
-      _putchar(c[i]);
-      
-    }
-  
-  _putchar('\n');
-  
-  return (0);
-  
-=======
-/**
- * main - prints the string "Holberton" from a character array.
  * Return: 0 on success.
  */
 int main(void)
@@ -45,5 +13,4 @@ int main(void)
 	}
 	_putchar('\n');
 	return (0);
->>>>>>> 3c71142e3f33f101caf6eeb1838cacab70e8f0d1
 }
