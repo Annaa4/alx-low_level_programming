1b49d0a1601a05e2f44fdcce1104a6bd3980273d
Anna <annasow296@gmail.com>
2022-09-13 06:19:54 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,10 @@
-#include "alx.h"
-<<<<<<< HEAD
+#include "holberton.h"
 
 /**
 
- * Return: 0 on success.
+ *main - prints Holberton
+
+ *Return: 0
 
  */
 
@@ -11,39 +12,26 @@ int main(void)
   
 {
   
-  char c[] = "Alx";
+  _putchar('H');
   
-  int i;
+  _putchar('o');
   
-
+  _putchar('l');
   
-  for (i = 0; i < 9; i++)
-    
-    {
-      
-      _putchar(c[i]);
-      
-    }
+  _putchar('b');
+  
+  _putchar('e');
+  
+  _putchar('r');
+  
+  _putchar('t');
+  
+  _putchar('o');
+  
+  _putchar('n');
   
   _putchar('\n');
   
   return (0);
   
-=======
-/**
- * main - prints the string "Holberton" from a character array.
- * Return: 0 on success.
- */
-int main(void)
-{
-	char c[] = "Alx";
-	int i;
-
-	for (i = 0; i < 9; i++)
-	{
-		_putchar(c[i]);
-	}
-	_putchar('\n');
-	return (0);
->>>>>>> 3c71142e3f33f101caf6eeb1838cacab70e8f0d1
 }
