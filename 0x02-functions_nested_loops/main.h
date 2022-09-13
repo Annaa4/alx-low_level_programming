3d3641c24b7b0e4f17e2b42b82c96e438f2d6b07
Anna Ndiaye <108986417+Annaa4@users.noreply.github.com>
2022-09-13 14:46:25 +0000

    Update main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,38 +1,4 @@
 #ifndef ALX_A
-<<<<<<< HEAD
-
-#define ALX_A
-
-
-
-char _putchar(char c);
-
-void print_alphabet(void);
-
-void print_alphabet_x10(void);
-
-int _islower(int c);
-
-int _isalpha(int c);
-
-int print_sign(int n);
-
-int _abs(int);
-
-int print_last_digit(int nld);
-
-void jack_bauer(void);
-
-void times_table(void);
-
-int add(int one, int two);
-
-void print_to_98(int n);
-
-void print_times_table(int n);
-
-
-=======
 #define ALX_A
 
 char _putchar(char c);
@@ -49,5 +15,4 @@ int add(int one, int two);
 void print_to_98(int n);
 void print_times_table(int n);
 
->>>>>>> 1b55f063267ade778f0c8d934793ac013c1e637c
 #endif /* ALX_H*/
