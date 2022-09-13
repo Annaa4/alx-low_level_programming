1740bbc128f897508b523df10f33ddf7805d6746
Anna <annasow296@gmail.com>
2022-09-13 06:03:07 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,31 @@
+#include "alx.h"
+
+/**
+
+ * Return: 0 on success.
+
+ */
+
+int main(void)
+  
+{
+  
+  char c[] = "Alx";
+  
+  int i;
+  
+
+  
+  for (i = 0; i < 9; i++)
+    
+    {
+      
+      _putchar(c[i]);
+      
+    }
+  
+  _putchar('\n');
+  
+  return (0);
+  
+}
