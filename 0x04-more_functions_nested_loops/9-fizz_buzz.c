cce3927003947c8620891c418b62c71c0378d850
Anna Ndiaye <108986417+Annaa4@users.noreply.github.com>
2022-09-15 13:01:18 +0000

    Update 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,85 +1,4 @@
 #include <stdio.h>
-<<<<<<< HEAD
-
-/**
-
- *
-
- * Return: void.
-
- */
-
-
-
-int main(void)
-  
-{
-  
-
-  
-  int i = 1;
-  
-
-  
-  while (i <= 100)
-    
-    {
-      
-      if (i % 3 == 0 && i % 5 == 0)
-	
-	{
-	  
-	  printf("FizzBuzz");
-	  
-	}
-      
-      else if (i % 3 == 0)
-	
-	{
-	  
-	  printf("Fizz");
-	  
-	}
-      
-      else if (i % 5 == 0)
-	
-	{
-	  
-	  printf("Buzz");
-	  
-	}
-      
-      else
-	
-	{
-	  
-	  printf("%i", i);
-	  
-	}
-      
-
-      
-      if (i != 100)
-	
-	{
-	  
-	  putchar(' ');
-	  
-	}
-      
-
-      
-      i++;
-      
-    }
-  
-  putchar('\n');
-  
-  return (0);
-  
-
-  
-=======
 /**
  *
  * Return: void.
@@ -119,5 +38,4 @@ int main(void)
 	putchar('\n');
 	return (0);
 
->>>>>>> 7d7d312f370727fd71876a4ece45cf591f044f74
 }
