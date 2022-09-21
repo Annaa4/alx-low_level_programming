4a9c6da06223e5ad3d56b68c03c0d6d53145a284
Anna Ndiaye <108986417+Annaa4@users.noreply.github.com>
2022-09-21 23:18:12 +0000

    Update 7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,45 +1,3 @@
-<<<<<<< HEAD
-#include "holberton.h"
-
-/**
-
- * *leet - encodes a string into 1337
-
- *
-
- * @c: char to check
-
- *
-
- * Return: 0 is success
-
- */
-
-char *leet(char *c)
-  
-{
-  
-  char a[] = "aeotlAEOTL";
-  
-  char b[] = {'4', '3', '0', '7', '1',
-	      
-	      '4', '3', '0', '7', '1'};
-  
-  int x, y;
-  
-
-  
-  for (x = 0; c[x] != '\0'; x++)
-    
-    for (y = 0; a[y] != '\0'; y++)
-      
-      if (c[x] == a[y])
-	
-	c[x] = b[y];
-  
-  return (c);
-  
-=======
 #include "main.h"
 /**
  * *leet - encodes a string into 1337
@@ -60,5 +18,4 @@ char *leet(char *c)
 			if (c[x] == a[y])
 				c[x] = b[y];
 	return (c);
->>>>>>> bf65963043e37e87cc90b5c2f799500f3b818b83
 }
