dd58d2b99c2b6a2bbeb18bf8dfcb591f864bd6c6
Anna Ndiaye <108986417+Annaa4@users.noreply.github.com>
2022-09-21 23:20:51 +0000

    Update 100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,49 +1,4 @@
 #include "main.h"
-<<<<<<< HEAD
-
-/**
-
- * *rot13 - encodes a string using rot13
-
- *
-
- * @rot: int to check
-
- *
-
- * Return: 0 is success
-
- */
-
-char *rot13(char *rot)
-  
-{
-  
-  int a, b;
-  
-  char input[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-  
-  char output[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
-  
-
-  
-  for (a = 0; rot[a] != '\0'; a++)
-    
-    for (b = 0; input[b] != '\0'; b++)
-      
-      if (rot[a] == input[b])
-	
-	{
-	  
-	  rot[a] = output[b];
-	  
-	  break;
-	  
-	}
-  
-  return (rot);
-  
-=======
 /**
  * *rot13 - encodes a string using rot13
  *
@@ -65,5 +20,4 @@ char *rot13(char *rot)
 				break;
 			}
 	return (rot);
->>>>>>> 249c16047beac13b00fb489adc97b5e16069a488
 }
