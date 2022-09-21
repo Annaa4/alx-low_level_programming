fab82d1ab3f2fd738bb3127e29e39a6afae6b9db
Anna Ndiaye <108986417+Annaa4@users.noreply.github.com>
2022-09-21 23:11:19 +0000

    Update 4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,43 +1,4 @@
 #include "main.h"
-<<<<<<< HEAD
-
-/**
-
- * reverse_array - reverse the content of an array of intergers
-
- *
-
- * @a: pointer to int to check
-
- * @n: int to check
-
- * Return: 0 is success
-
- */
-
-void reverse_array(int *a, int n)
-  
-{
-  
-  int x, y, z;
-  
-
-  
-  for ((x = 0, y = n - 1); x < y; x++, y--)
-    
-    {
-      
-      z = a[x];
-      
-      a[x] = a[y];
-      
-      a[y] = z;
-      
-
-      
-    }
-  
-=======
 /**
  * reverse_array - reverse the content of an array of intergers
  *
@@ -56,5 +17,4 @@ void reverse_array(int *a, int n)
 		a[y] = z;
 
 	}
->>>>>>> 0d307840027d5d11de8b54b46c3dfb4609a78a71
 }
