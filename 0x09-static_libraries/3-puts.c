a06c9b8abc9368d75ab6b2d3362d7fddc49ae0cd
Anna Ndiaye <108986417+Annaa4@users.noreply.github.com>
2022-09-30 21:11:26 +0000

    Update 3-puts.c

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -12,6 +12,6 @@ void _puts(char *str)
 	int string;
 
 	for (string = 0; *(str + string) != '\0'; string++)
-		_putchar(*(str + string));
-	_putchar('\n');
+		putchar(*(str + string));
+	putchar('\n');
 }
