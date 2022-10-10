f69d226efbc489f5d64ebe1a86dd27f02207ec81
Anna Ndiaye <108986417+Annaa4@users.noreply.github.com>
2022-10-10 11:42:52 +0000

    Update 5-free_dog.c

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,31 +1,16 @@
 #include "dog.h"
 
-
-
 /**
-
- *free_dog - Frees dog
-
- *@d: dog to be freed
-
- *
-
- */
-
+*free_dog - Frees dog
+*@d: dog to be freed
+*
+*/
 void free_dog(dog_t *d)
-  
 {
-  
-  if (d == NULL)
-    
-    return;
-  
+if (d == NULL)
+return;
 
-  
-  free(d->name);
-  
-  free(d->owner);
-  
-  free(d);
-  
+free(d->name);
+free(d->owner);
+free(d);
 }
