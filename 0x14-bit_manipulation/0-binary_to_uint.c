bacf728ceff671284290c6bc3bfa0dffb287a7c3
Anna Ndiaye <108986417+Annaa4@users.noreply.github.com>
2022-10-27 21:17:29 +0000

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,10 +1,12 @@
 #include "main.h"
+
 /**
  * binary_to_uint - converts a binary to an unsigned int
  * @b: the binary number as a string
  *
  * Return: the converted value
- */
+*/
+
 unsigned int binary_to_uint(const char *b)
 {
 unsigned int decimal = 0;
@@ -31,6 +33,7 @@ return (decimal);
  *
  * Return: 1 if string is valid, 0 otherwise
  */
+
 int check_valid_string(const char *b)
 {
 if (b == NULL)
